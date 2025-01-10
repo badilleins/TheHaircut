1fdb9abfc7b5b2f55e7b1a73a1e582c54e4a4c35
Andres <aramirez7310@uta.edu.ec>
2025-01-09 21:30:36 -0500

    environmments added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCCOJPX46JW3x3sm7FLQV1sIoNxjDz4XF4",
+    authDomain: "citas-barber-28dfc.firebaseapp.com",
+    projectId: "citas-barber-28dfc",
+    storageBucket: "citas-barber-28dfc.appspot.com",
+    messagingSenderId: "584075629946",
+    appId: "1:584075629946:web:1017ab295cf5229afe72c3",
+    measurementId: "G-YQRCGZY214"
+  }
 };
